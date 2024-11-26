6dac83b1c5c600f7ccc3f293b8bfeb6cdda568f0
wilson <wilsona4135@gmail.com>
2024-11-26 22:37:15 +0800

    more iterator helpers

--- src/lib.rs
+++ src/lib.rs
@@ -138,6 +138,24 @@ pub trait IteratorHelper where Self: Sized {
     unsafe fn variants_unchecked<E: Enumeration>(self) -> impl Iterator<Item = E> where Self: IntoIterator<Item = E::Index> {
         self.into_iter().map(|i| unsafe { E::variant_unchecked(i) })
     }
+
+    #[inline(always)]
+    /// Convert index into variant without checking
+    fn values<E: Enumeration>(self) -> impl Iterator<Item = (E, &'static E::AssociatedValueType)> where Self: IntoIterator<Item = E> {
+        self.into_iter().map(|e| (e, e.value()))
+    }
+
+    #[inline(always)]
+    /// Convert index into variant without checking
+    fn index_values<E: Enumeration>(self) -> impl Iterator<Item = (E::Index, &'static E::AssociatedValueType)> where Self: IntoIterator<Item = E> {
+        self.into_iter().map(|e| (e.to_index(), e.value()))
+    }
+
+    #[inline(always)]
+    /// Convert index into variant without checking
+    fn usize_values<E: Enumeration>(self) -> impl Iterator<Item = (usize, &'static E::AssociatedValueType)> where Self: IntoIterator<Item = E>, E::Index: Into<usize> {
+        self.into_iter().map(|e| (e.to_index().into(), e.value()))
+    }
 }
 
 impl<T: Iterator> IteratorHelper for T {}
